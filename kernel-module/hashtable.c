bcd649eec6cbb60e3fe1101d371c186d474383ba
Matej Minarik <matejminarik.fiit@gmail.com>
2015-02-21 15:40:01 +0100

    Procfs from demo

--- kernel-module/hashtable.c
+++ kernel-module/hashtable.c
@@ -2,12 +2,21 @@
 #include <linux/kernel.h>
 #include <linux/hashtable.h>
 #include <linux/slab.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>	/* for copy_from_user */
+
+#define PROCF_MAX_SIZE 1024
+#define PROCF_NAME "linux-kernel-firewall"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("linux-simple-firewall");
 MODULE_AUTHOR("Liu Feipeng/roman10");
 
 DEFINE_HASHTABLE(hashmap, 4);
+
+static struct proc_dir_entry *mf_proc_file;
+unsigned long procf_buffer_pos;
+char *procf_buffer;
 
 struct user_hash {
 	struct hlist_node hash;
@@ -20,12 +29,55 @@ struct user_hash {
 	unsigned short dst_port;
 };
 
+int procf_write(struct file *file, const char *buffer, 
+	unsigned long count, void *data)
+{
+   int i, j;
+   struct mf_rule_desp *rule_desp;
+   char c;
+ 
+   printk(KERN_INFO "procf_write is called.\n");
+ 
+   /*read the write content into the storage buffer*/
+ 
+   procf_buffer_pos = 0;
+   printk(KERN_INFO "pos: %ld; count: %ld\n", procf_buffer_pos, count);
+   if (procf_buffer_pos + count > PROCF_MAX_SIZE) {
+ 
+       count = PROCF_MAX_SIZE-procf_buffer_pos;
+ 
+   } 
+   if (copy_from_user(procf_buffer+procf_buffer_pos, buffer, count)) {
+ 
+       return -EFAULT;
+ 
+   }
+
+   printk("Received: ");
+   while((c = procf_buffer[procf_buffer_pos]) != '\n') {
+   		printk("%c", c);
+   }
+}
+
 int init_module(){
 	struct user_hash *node;
 	struct user_hash *rule = kmalloc(sizeof(struct user_hash), GFP_KERNEL);
 	unsigned int bkt = 0;
 	unsigned int cnt = 0;
 	struct hlist_node empty = {.next = NULL, .pprev = NULL};
+
+	procf_buffer = (char *) vmalloc(PROCF_MAX_SIZE);
+	mf_proc_file = create_proc_entry(PROCF_NAME, 0644, NULL);
+	if(mf_proc_file == NULL) {
+		printk("Could not initialize /proc/%s\n", PROCF_NAME);
+		return -ENOMEM;
+	}
+
+	//mf_proc_file->read_proc = procf_read;
+ 
+    mf_proc_file->write_proc = procf_write;
+ 
+    printk(KERN_INFO "/proc/%s is created\n", PROCF_NAME);
 
 	printk("firewall init\n");
 
--- parser/firewall.c
+++ parser/firewall.c
@@ -13,12 +13,37 @@ extern "C" FILE *yyin;
 extern int line_num;
 extern void yyerror(const char *s);
 
+#define PROCFILE "/proc/linux-kernel-firewall"
+
+void send_to_proc(char *str) {
+	FILE *fw;
+	fw = fopen(PROCFILE, "w");
+	if(fw == NULL) {
+		fprintf(stderr, "Cannot open %s\n", PROCFILE);
+		exit(-1);
+	}
+
+	fprintf(fw, "%s", str);
+	fclose(fw);
+}
+
+void send_rule_to_proc(struct rule_t rule) {
+	char srule[200];
+	sprintf(srule, "%d %s %s %s %s %d %d\n",
+		rule.id, rule.action, rule.proto,
+		rule.src_ip, rule.dst_ip,
+		rule.src_port, rule.dst_port);
+
+	fprintf(stdout, "Sending %s", srule);
+	send_to_proc(srule);
+}
+
 void yyerror(const char *s) {
        cout << "EEK, parse error!  Message: " << s << " on line: " << line_num;
        exit(-1);
 }
 
-void parseRules(){
+void parse_rules(){
 	FILE *myfile = fopen("rules.in", "r");
 	if (!myfile) {
 		cout << "I can't open a.snazzle.file!" << endl;
@@ -33,8 +58,14 @@ void parseRules(){
 	} while (!feof(yyin));	
 }
 
-void printRules(){
-	for(std::list<rule_t>::iterator i = rulesList.begin(); i != rulesList.end(); ++i){
+void send_rules() {
+	for(std::list<rule_t>::iterator i = rulesList.begin(); i != rulesList.end(); ++i) {
+		send_rule_to_proc(*i);
+	}
+}
+
+void print_rules(){
+	for(std::list<rule_t>::iterator i = rulesList.begin(); i != rulesList.end(); ++i) {
 		cout << i->id << " ";
 		cout << i->action << " ";
 		cout << i->proto << " ";
@@ -48,13 +79,14 @@ void printRules(){
 
 int main(int argc, char *argv[]){
 
-	parseRules();
+	parse_rules();
+	send_rules();
 
 	int opt;
 	while((opt = getopt(argc, argv, "pa:d:f:")) != -1) {
 		switch(opt) {
 			case 'p': /* print rules */
-				printRules();
+				print_rules();
 				break;
 			case 'a': /* add rule */
 				break;
@@ -63,7 +95,7 @@ int main(int argc, char *argv[]){
 			case 'f': /* read rules from file */
 				break;
 			default: /* unmatched argument */
-				fprintf(stderr, "Usage: %s <-a rule | -p | -d rule-id | -f file>\n", 
+				fprintf(stderr, "Usage: %s -a rule | -p | -d rule-id | -f file>\n", 
 					argv[0]);
 		}
 	}
