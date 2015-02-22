2156036daf9b4e49ea97a0009b01d33790bf013c
Matej Minarik <matejminarik.fiit@gmail.com>
2015-02-22 23:10:10 +0100

    Added simple filtering to kernel module

--- kernel-module/hashtable.c
+++ kernel-module/hashtable.c
@@ -8,6 +8,11 @@
 #include <linux/in.h>
 #include <linux/fs.h>
 #include <linux/string.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <linux/ip.h>
 
 #define LINE_MAX_SIZE 50
 #define PROCFS_MAX_SIZE 1024
@@ -20,6 +25,11 @@ MODULE_AUTHOR("Matej Minarik");
 
 DEFINE_HASHTABLE(hashmap, 4);
 
+//the structure used to register the function
+static struct nf_hook_ops nfho;
+static struct nf_hook_ops nfho_out;
+
+//variables for procfs
 static struct proc_dir_entry *procfs;
 static char procfs_buffer[PROCFS_MAX_SIZE];
 static unsigned long procfs_buffer_size = 0;
@@ -169,6 +179,64 @@ static ssize_t procfs_write(struct file *file, const char *buffer, unsigned long
 	return procfs_buffer_size;
 }
 
+unsigned int hook_func_in(unsigned int hooknum, struct sk_buff *skb, 
+        const struct net_device *in, const struct net_device *out,
+        int (*okfn)(struct sk_buff *)) {
+ 	unsigned int proto_key = 0;
+ 	struct user_hash *node;
+
+   /*get src address, src netmask, src port, dest ip, dest netmask, dest port, protocol*/
+   struct iphdr *ip_header = (struct iphdr *)skb_network_header(skb);
+   struct udphdr *udp_header;
+   struct tcphdr *tcp_header;
+ 
+   /**get src and dest ip addresses**/
+   unsigned int src_ip = (unsigned int)ip_header->saddr; 
+   unsigned int dest_ip = (unsigned int)ip_header->daddr; 
+   unsigned int src_port = 0;
+   unsigned int dest_port = 0;
+ 
+   /***get src and dest port number***/
+   if (ip_header->protocol == 17) { /* UDP */
+       udp_header = (struct udphdr *)(skb_transport_header(skb)+20);
+       src_port = (unsigned int)ntohs(udp_header->source);
+       dest_port = (unsigned int)ntohs(udp_header->dest);
+       proto_key = 2500;
+   } else if (ip_header->protocol == 6) { /* TCP */
+       tcp_header = (struct tcphdr *)(skb_transport_header(skb)+20);
+       src_port = (unsigned int)ntohs(tcp_header->source);
+       dest_port = (unsigned int)ntohs(tcp_header->dest);
+       proto_key = 1000;
+   } else if (ip_header->protocol == 1 ) { /* ICMP */
+       proto_key = 3800;
+   }
+
+   hash_for_each_possible_rcu(hashmap, node, hash, proto_key) {
+   		printk("Possible rule-id %u\n", node->id);
+   		printk("node->src_ip src_ip %pI4h %pI4h\n", &(node->src_ip), &src_ip);
+   		if(node->src_ip == 0 || node->src_ip == src_ip) {
+   			/* TODO verify */
+   			if(node->dst_ip == 0 || node->dst_ip == dest_ip) {
+   				/* TODO verify */
+   				if(node->action == 'd'){
+   					#ifdef DBG
+   					printk("packet drop\n");
+   					#endif
+   					return NF_DROP;
+   				}
+   			}
+   		}
+   }
+ 
+ #ifdef DBG
+   printk("IN packet info: src ip: %u, src port: %u; dest ip: %u, dest port: %u; proto: %u\n", 
+    src_ip, src_port, dest_ip, dest_port, ip_header->protocol); 
+ #endif
+
+   return NF_ACCEPT;                
+ 
+}
+
 int init_module(){
 	static const struct file_operations proc_file_fops = {
 		.owner = THIS_MODULE,
@@ -183,8 +251,17 @@ int init_module(){
 		return -ENOMEM;
 	}
 
+	/* Fill in the hook structure for incoming packet hook*/
+    nfho.hook = hook_func_in;
+    nfho.hooknum = NF_INET_LOCAL_IN;
+    nfho.pf = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST;
+ 
+    nf_register_hook(&nfho);         // Register the hook
+
 	printk("firewall init\n");
-	printk(KERN_INFO "/proc/%s created\n", PROCFS_NAME);
+	printk("/proc/%s created\n", PROCFS_NAME);
+	printk("hook_func_in registered\n");
 
 	return 0;
 }
@@ -210,6 +287,9 @@ void cleanup_module(){
 #endif
 	printk("procfs cleanup\n");
 	remove_proc_entry(PROCFS_NAME, NULL);
+
+	printk("hook_func_in unregister\n");
+	nf_unregister_hook(&nfho);
 	
 	printk("hashmap cleanup\n");
 	node = NULL;
